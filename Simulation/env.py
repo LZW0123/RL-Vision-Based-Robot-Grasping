377e9ee63e3c8884580b1a1a6df2ec3629c9b812
LZW0123 <1589598459@qq.com>
2022-05-22 20:54:14 +0800

    add forward kinematic

--- Simulation/env.py
+++ Simulation/env.py
@@ -1,7 +1,7 @@
 import sim
 import numpy as np
 import math
-
+from math import cos,sin,radians
 
 class Dobot:
 
@@ -99,8 +99,7 @@ class Dobot:
         joint_handle = self.joint_handle
         sim.simxSetJointTargetPosition(clientID, joint_handle[num], self.angle2radian(angle), sim.simx_opmode_oneshot)
        
-        
-        
+              
 
 
     def radian2angle(self, radian):
@@ -111,6 +110,57 @@ class Dobot:
         radian=angle*(math.pi/180)
         return radian
 
+class Kinematics:
+    def __init__(self): # 仅考虑旋转关节
+        self.DH_alpha=np.array([0,90,0,90]).reshape((1,4))
+        self.DH_a=np.array([0,0,0.135,0.2067]).reshape((1,4))
+        self.DH_d=np.array([0,0,0,0]).reshape((1,4))
+        
+        
+    # DH建模后相邻两杆的单级齐次变换矩阵（采用MDH）
+    def T_mat_DH(self,alpha,a,d,theta):
+        alpha = radians(alpha)
+        theta = radians(theta)              #由角度变为弧度
+        matrix = np.mat(np.zeros((4, 4)))   
+        matrix[0,0] = cos(theta)
+        matrix[0,1] = -sin(theta)
+        matrix[0,3] = a
+        matrix[1,0] = sin(theta) * cos(alpha)
+        matrix[1,1] = cos(theta) * cos(alpha)
+        matrix[1,2] = -sin(alpha)
+        matrix[1,3] = -sin(alpha) * d
+        matrix[2,0] = sin(theta) * sin(alpha)
+        matrix[2,1] = cos(theta) * sin(alpha)
+        matrix[2,2] = cos(alpha)
+        matrix[2,3] = cos(alpha) * d
+        matrix[3,3] = 1
+        return matrix
+
+    def DOF4_matrix(self,joints_angle):
+        DH_theta=np.array(joints_angle).reshape((1,4))
+        DH_Parameter_Table=np.concatenate((self.DH_alpha,self.DH_a,self.DH_d,DH_theta),axis=0)
+        DH_mat = DH_Parameter_Table.T
+        print(DH_mat)
+        DOF4_mat = np.identity(4)
+        for i in range(0,4):
+            temp_mat = self.T_mat_DH(DH_mat[i,0],DH_mat[i,1],DH_mat[i,2],DH_mat[i,3])
+            DOF4_mat = DOF4_mat * temp_mat
+        return DOF4_mat
+
+    def ForwardKinematics(self,joints_angle):
+        DOF4_mat=self.DOF4_matrix(joints_angle)
+        x=DOF4_mat[0,3]
+        y=DOF4_mat[1,3]
+        z=DOF4_mat[2,3]
+        return [x,y,z]
+
+    def InverseKinematics(self,ee_pos):
+        joints_angles=1
+        return joints_angles
+
 if __name__ == '__main__':
-    robot = Dobot()
-    robot.TestRobotRun(0,90)
+    # robot = Dobot()
+    # robot.TestRobotRun(0,90)
+    kinematics=Kinematics()
+    position=kinematics.ForwardKinematics([0,0,0,0])
+    print(position)
